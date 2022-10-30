c36167f0460e01a0595dfecaf0faceff8b5aca17
= <=>
2022-10-30 23:35:13 +0900

    10/30 algolithm

---
+++ 섹션5/1. 두 배열 합치기.js
@@ -0,0 +1,12 @@
+function solution(arr1, arr2) {
+  // sort는 n log n의 시간복잡도를 가진다.
+  // const answer = [...arr1, ...arr2].sort((a, b) => a - b);
+  // [투포인터 알고리즘]
+  // 두개의 포인터를 가지고 for문 한개 안에서 정답을 찾을 수 있다.
+  const answer = [];
+  return answer;
+}
+
+let a = [1, 3, 5];
+let b = [2, 3, 6, 7, 9];
+console.log(solution(a, b));
